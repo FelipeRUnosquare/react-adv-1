1f126610f39f47c984f2065d86d15469a576b64e
FelipeRUnosquare <felipe.ramirez@unosquare.com>
2022-09-16 15:35:29 -0500

    control props implementado

--- src/02-components-patterns/components/ProductCard.tsx
+++ src/02-components-patterns/components/ProductCard.tsx
@@ -5,21 +5,34 @@
 import { createContext, ReactElement } from "react";
 import { useProduct } from "../hooks/useProduct";
 import styles from "../styles/styles.module.css";
-import { Product, ProductContextProps } from "../interfaces/interfaces";
+import {
+  Product,
+  ProductContextProps,
+  onChangeArgs,
+} from "../interfaces/interfaces";
 import { CSSProperties } from "react";
 
 export interface Props {
   className?: string;
   children?: ReactElement | ReactElement[];
+  onChange?: (args: onChangeArgs) => void;
   product: Product;
   style?: CSSProperties;
+  value?: number;
 }
 
 export const ProductContext = createContext({} as ProductContextProps);
 const { Provider } = ProductContext;
 
-export const ProductCard = ({ children, product, className, style }: Props) => {
-  const { counter, increaseBy } = useProduct();
+export const ProductCard = ({
+  children,
+  product,
+  className,
+  style,
+  onChange,
+  value,
+}: Props) => {
+  const { counter, increaseBy } = useProduct({ product, onChange, value });
   return (
     <Provider
       value={{
